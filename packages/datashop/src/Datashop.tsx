a07f33d7c39751fb21e4cc864ce94bbbcdf0813f
lmalunin <lmalunin@gmail.com>
2023-12-21 01:16:28 +0300

    component to lib added

--- packages/datashop/src/Datashop.tsx
+++ packages/datashop/src/Datashop.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./index.scss";
 import addNumbers from "@workspace/libs/src";
+import { Somecomponent } from '@workspace/libs/src/somecomponent'
 
 const Datashop = ({ dispatch }) => (
   <div className="dashboard">
     <p>Remote Datashop</p>
-    {addNumbers(5, 6)}
+    {addNumbers(5, 7)}
+    <br/>
+    <Somecomponent title={'123'}/>
+    <br/>
     <button
       onClick={() => {
         dispatch({
---
+++ packages/libs/src/somecomponent.tsx
@@ -0,0 +1,7 @@
+export const Somecomponent = (props) => {
+  return (
+    <>
+      {props?.title}
+    </>
+  )
+}
