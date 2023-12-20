67738f61b832cec4cbe5863f9a5b81854c81f514
lmalunin <lmalunin@gmail.com>
2023-12-20 23:44:28 +0300

    switched to @workspace

--- packages/datashop/src/Datashop.tsx
+++ packages/datashop/src/Datashop.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.scss";
-import addNumbers from "@datashop/libs/src";
+import addNumbers from "@workspace/libs/src";
 
 const Datashop = ({ dispatch }) => (
   <div className="dashboard">
